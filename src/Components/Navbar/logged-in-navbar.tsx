e456e003c87ca598138b51b767fbc2553b841c52
vladradin <vlad.radin@gmail.com>
2019-02-06 23:22:27 +0200

    asaa

--- src/Components/Navbar/logged-in-navbar.tsx
+++ src/Components/Navbar/logged-in-navbar.tsx
@@ -19,32 +19,21 @@ class LoggedInNavbar extends React.Component<LoggedInNavbarProps, LoggedInNavbar
     }
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-success">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link to={path.coursesUrl} className="nav-link">Courses</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to={path.teachersUrl} className="nav-link">Teachers</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to={path.studentsUrl} className="nav-link">Students</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to={path.usersUrl} className="nav-link">Users</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to={path.administratorUrl} className="nav-link">Administrator</Link>
-                        </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-                        <i className="fa fa-user-circle fa-2x mx-1"></i>
-                    </form>
-                </div>
+            <nav>
+                <ul className="navbar-h">
+                    <li className="nav-link">
+                        <Link to="courses">Cursuri</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link to="teachers">Profesori</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link to="students">Elevi</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link to="users">Utilizatori</Link>
+                    </li>
+                </ul>
             </nav>
         );
     }
--- src/Components/Navbar/logged-out-navbar.tsx
+++ src/Components/Navbar/logged-out-navbar.tsx
@@ -15,35 +15,7 @@ class LoggedOutNavbar extends Component<LoggedOutNavbarProps, LoggedOutNavbarSta
         this.state = { user: {} };
     }
     render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link to="/courses" className="nav-link">Courses</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/courses" className="nav-link">Teachers</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/courses" className="nav-link">Students</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/courses" className="nav-link">Users</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link to="/courses" className="nav-link">Administrator</Link>
-                        </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        <i className="fa fa-user-circle fa-lg"></i>
-                    </form>
-                </div>
-            </nav>
-        );
+        return null;       
     }
 }
 
